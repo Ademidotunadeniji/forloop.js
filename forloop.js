ca20bbb47d648ba869f53498fd0e840270130bfa
Ademidotunadeniji <ademidotunadeniji32@gmail.com>
2024-12-10 22:42:02 +0100

    first commit

---
+++ forloop.js
@@ -0,0 +1,64 @@
+// const number = 10
+// if(number % 2 === 0){
+//     console.log('number is even')
+// }
+
+// const number = 5
+// if(number % 2 === 0){
+//     console.log('number is even')
+// }
+// else{
+//     console.log('number is odd')
+// }
+
+// const side1 = 3
+// const side2 = 3
+// const side3 = 3
+// if(side1 === side2 === side3)
+// console.log('Equilateral triangle')
+// }else if(side1 === side2 || side1 === side3 || side2 === side3){
+//     console.log('Isoceles triangle')
+// }
+// else{
+//     console.log('scalene triangle')
+// }
+
+//const side1='equalateral'
+//console.log(side1)
+
+//const side2= 'isosceles triangle'
+//console.log(side2)
+
+//const side3='scalene triangle'
+//console.log(side3)
+
+
+// const side1='equalateral'
+// console.log(side1)
+
+// const side2= 'isosceles triangle'
+// console.log(side2)
+
+// const side3='scalene triangle'
+// console.log(side3)
+
+// if ('side1 === side2 && side2 === side3') {
+//    console.log(side3)
+// }
+
+//  const age= 19
+//  if (age%2){
+//      console.log("age is odd")
+//  } else{
+//      console.log ("age is even")
+//      }
+
+const number= 14
+if(number % 2 ===0){
+    console.log("number is even")
+}
+else{
+    console.log("number is odd")
+}
+
+
